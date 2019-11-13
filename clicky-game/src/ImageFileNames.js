45031453c999dae8a8f06723a77ad0a802cb06c0
Brian <briancalderon200@gmail.com>
2019-11-12 19:45:47 -0800

    react files

---
+++ clicky-game/src/ImageFileNames.js
@@ -0,0 +1,20 @@
+let ImageFileNames = [
+  "challenger.jpg",
+  "judge.jpg",
+  "mustang.jpg",
+  "charger.jpg",
+  "cuda.jpg",
+  "barricuda.jpg",
+  "transam.jpg",
+  "firebird.jpg",
+  "442.jpg",
+  "gt500.jpg",
+  "elcamino.jpg",
+  "hudson.jpg",
+  "gto.jpg",
+  "chevelle.jpg",
+  "cobra.jpg",
+  "torino.jpg"
+];
+
+export default ImageFileNames;
---
+++ clicky-game/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>
+      UCDavis Coding Bootcamp © 2019 Copyright:&nbsp;
+      <a href="" target="_blank" rel="noopener noreferrer">
+        Brian Calderon
+      </a>
+    </span>
+  </footer>
+);
+
+export default Footer;
