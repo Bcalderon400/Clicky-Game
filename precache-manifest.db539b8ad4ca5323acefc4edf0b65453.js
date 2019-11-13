a6e4386aced49756e4e4f249014702dfa1cccdb9
Brian <briancalderon200@gmail.com>
2019-11-12 20:16:10 -0800

    Updates

---
+++ precache-manifest.db539b8ad4ca5323acefc4edf0b65453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef655f9613e32e9a444d4f8c61983698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a54d423fe674d4da458",
+    "url": "/static/css/main.9324c1b4.chunk.css"
+  },
+  {
+    "revision": "e49e35516a4f78219438",
+    "url": "/static/js/2.f60726b2.chunk.js"
+  },
+  {
+    "revision": "6a54d423fe674d4da458",
+    "url": "/static/js/main.b39ae05e.chunk.js"
+  },
+  {
+    "revision": "acc157da072edc658c45",
+    "url": "/static/js/runtime-main.c647743d.js"
+  }
+]);
